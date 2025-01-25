79323dfaf2c72edb9268732ebbd3f7908d6b4dd1
Natsis Thanasis <natsisathan@gmail.com>
2025-01-25 10:13:52 +0200

    Commit 2025-01-25 10:13

--- git_analysys.R
+++ git_analysys.R
@@ -43,3 +43,316 @@
 # https://www.r-bloggers.com/2018/03/guide-to-tidy-git-analysis/
 
 
+#+ include=F
+
+#'
+#' **QCRad T03**
+#'
+#' **Details and source code: [`github.com/thanasisn/BBand_LAP`](https://github.com/thanasisn/BBand_LAP)**
+#'
+#' **Data display: [`thanasisn.github.io`](https://thanasisn.github.io/)**
+#'
+
+#+ include=F
+## __ Document options  --------------------------------------------------------
+knitr::opts_chunk$set(comment   = ""      )
+knitr::opts_chunk$set(dev       = "png"   )
+knitr::opts_chunk$set(out.width = "100%"  )
+knitr::opts_chunk$set(fig.align = "center")
+knitr::opts_chunk$set(fig.cap   = " empty caption ")
+knitr::opts_chunk$set(fig.pos   = '!h'    )
+knitr::opts_chunk$set(tidy = TRUE,
+                      tidy.opts = list(
+                        indent       = 4,
+                        blank        = FALSE,
+                        comment      = FALSE,
+                        args.newline = TRUE,
+                        arrow        = TRUE)
+)
+
+## __ Set environment  ---------------------------------------------------------
+closeAllConnections()
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name  <- "~/CODE/git_analysis/git_analysys.R"
+
+if (!interactive()) {
+  pdf( file = paste0("~/CODE/git_analysis/RUNTIME/",   basename(sub("\\.R$", ".pdf", Script.Name))))
+}
+
+
+## __ Load libraries  ----------------------------------------------------------
+library(data.table, warn.conflicts = FALSE, quietly = TRUE)
+library(dplyr,      warn.conflicts = FALSE, quietly = TRUE)
+library(glue,       warn.conflicts = FALSE, quietly = TRUE)
+library(stringr,    warn.conflicts = FALSE, quietly = TRUE)
+library(forcats,    warn.conflicts = FALSE, quietly = TRUE)
+library(tidygraph,  warn.conflicts = FALSE, quietly = TRUE)
+library(ggraph,     warn.conflicts = FALSE, quietly = TRUE)
+library(tidytext,   warn.conflicts = FALSE, quietly = TRUE)
+
+
+#+ include=T, echo=F, results="asis"
+##  Variables  -----------------------------------------------------------------
+
+repodir <- "~/BBand_LAP/"
+
+system(glue('git -C {repodir} log -3'))
+
+log_format_options <- c(datetime = "cd", commit = "h", parents = "p", author = "an", subject = "s")
+option_delim <- "\t"
+log_format   <- glue("%{log_format_options}") %>% collapse(option_delim)
+log_options  <- glue('--pretty=format:"{log_format}" --date=format:"%Y-%m-%d %H:%M:%S"')
+log_cmd      <- glue('git -C {repodir} log {log_options}')
+log_cmd
+
+system(glue('{log_cmd} -3'))
+
+history_logs <- system(log_cmd, intern = TRUE) %>%
+  str_split_fixed(option_delim, length(log_format_options)) %>%
+  as_tibble() %>%
+  setNames(names(log_format_options))
+
+
+history_logs <- history_logs %>%
+  mutate(parents = str_split(parents, " "))
+
+
+# Start with NA
+history_logs <- history_logs %>% mutate(branch = NA_integer_)
+
+# Create a boolean vector to represent free columns (1000 should be plenty!)
+free_col <- rep(TRUE, 1000)
+
+for (i in seq_len(nrow(history_logs) - 1)) { # - 1 to ignore root
+  # Check current branch col and assign open col if NA
+  branch <- history_logs$branch[i]
+
+  if (is.na(branch)) {
+    branch <- which.max(free_col)
+    free_col[branch] <- FALSE
+    history_logs$branch[i] <- branch
+  }
+
+  # Go through parents
+  parents <- history_logs$parents[[i]]
+
+  for (p in parents) {
+    parent_col <- history_logs$branch[history_logs$commit == p]
+
+    # If col is missing, assign it to same branch (if first parent) or new
+    # branch (if other)
+    if (is.na(parent_col)) {
+      parent_col <- if_else(p == parents[1], branch, which.max(free_col))
+
+      # If NOT missing this means a split has occurred. Assign parent the lowest
+      # and re-open both cols (parent closed at the end)
+    } else {
+      free_col[c(branch, parent_col)] <- TRUE
+      parent_col <- min(branch, parent_col)
+
+    }
+
+    # Close parent col and assign
+    free_col[parent_col] <- FALSE
+    history_logs$branch[history_logs$commit == p] <- parent_col
+  }
+}
+
+history_logs %>%
+  count(author, sort = TRUE)
+
+
+istory_logs <- history_logs %>%
+  mutate(author = case_when(
+    str_detect(tolower(author), "hadley") ~ "Hadley Wickham",
+    str_detect(tolower(author), "kohske takahashi") ~ "Kohske Takahashi",
+    TRUE ~ str_to_title(author)
+  ))
+
+history_logs %>%
+  count(author) %>%
+  arrange(desc(n))
+
+history_logs %>%
+  count(author) %>%
+  top_n(10, n) %>%
+  mutate(author = fct_reorder(author, n)) %>%
+  ggplot(aes(author, n)) +
+  geom_col(aes(fill = n), show.legend = FALSE) +
+  coord_flip() +
+  theme_minimal() +
+  ggtitle("ggplot2 authors with most commits") +
+  labs(x = NULL, y = "Number of commits", caption = "Post by @drsimonj")
+
+
+# Convert commit to a factor (for ordering nodes)
+history_logs <- history_logs %>%
+  mutate(commit = factor(commit))
+
+# Nodes are the commits (keeping relevant info)
+nodes <- history_logs %>%
+  select(-parents) %>%
+  arrange(commit)
+
+# Edges are connections between commits and their parents
+edges <- history_logs %>%
+  select(commit, parents) %>%
+  unnest(parents) %>%
+  mutate(parents = factor(parents, levels = levels(commit))) %>%
+  transmute(from = as.integer(parents), to = as.integer(commit)) %>%
+  drop_na()
+
+# Create tidy directed graph object
+git_graph <- tbl_graph(nodes = nodes, edges = edges, directed = TRUE)
+
+git_graph
+#> # A tbl_graph: 3946 nodes and 4371 edges
+#> #
+#> # A directed acyclic simple graph with 1 component
+#> #
+#> # Node Data: 3,946 x 5 (active)
+#>   datetime            commit   author           subject             branch
+#>   <chr>               <fct>    <chr>            <chr>                <int>
+#> 1 2011-07-19 22:40:33 002e510d Kohske Takahashi remove style param…      4
+#> 2 2008-07-05 20:00:50 0036ad7f Hadley Wickham   HAcks to get tile …      1
+#> 3 2010-02-05 10:25:26 0072cf4c Hadley Wickham   Fix bug with empty…      1
+#> 4 2013-12-02 13:51:05 008018cc Josef Fruehwald  added stat_ellipse       9
+#> 5 2010-12-23 10:22:36 00821913 Hadley Wickham   Fix doc name             1
+#> 6 2011-12-23 06:24:28 008312c0 Hadley Wickham   Merge pull request…      1
+#> # ... with 3,940 more rows
+#> #
+#> # Edge Data: 4,371 x 2
+#>    from    to
+#>   <int> <int>
+#> 1  3070   968
+#> 2  1071   968
+#> 3  3370  1071
+#> # ... with 4,368 more rows
+
+
+
+git_graph %>%
+  ggraph() +
+  geom_edge_link(alpha = .1) +
+  geom_node_point(aes(color = factor(branch)), alpha = .3) +
+  theme_graph() +
+  theme(legend.position = "none")
+#> Using `nicely` as default layout
+
+ggraph_git <- . %>%
+  # Set node x,y coordinates
+  activate(nodes) %>%
+  mutate(x = datetime, y = branch) %>%
+  # Plot with correct layout
+  create_layout(layout = "manual", node.positions = as_tibble(activate(., nodes))) %>%
+  {ggraph(., layout = "manual") + theme_graph() + labs(caption = "Post by @drsimonj")}
+
+
+git_graph %>%
+  ggraph_git() +
+  geom_edge_link(alpha = .1) +
+  geom_node_point(aes(color = factor(branch)), alpha = .3) +
+  theme(legend.position = "none") +
+  ggtitle("Commit history of ggplot2")
+
+
+git_graph %>%
+  activate(nodes) %>%
+  filter(datetime > "2015-11-01", datetime < "2016-08-01") %>%
+  ggraph_git() +
+  geom_edge_link(alpha = .1) +
+  geom_node_point(aes(color = factor(branch)), alpha = .3) +
+  theme(legend.position = "none") +
+  ggtitle("Git history of ggplot2",
+          subtitle = "2015-11 to 2016-08")
+
+
+# 10 most-common authors
+top_authors <- git_graph %>%
+  activate(nodes) %>%
+  as_tibble() %>%
+  count(author, sort = TRUE) %>%
+  top_n(10, n) %>%
+  pull(author)
+
+# Plot
+git_graph %>%
+  activate(nodes) %>%
+  filter(datetime > "2015-11-01", datetime < "2016-08-01") %>%
+  mutate(author = factor(author, levels = top_authors),
+         author = fct_explicit_na(author, na_level = "Other")) %>%
+  ggraph_git() +
+  geom_edge_link(alpha = .1) +
+  geom_node_point(aes(color = author), alpha = .3) +
+  theme(legend.position = "bottom") +
+  ggtitle("ggplot2 commits by author",
+          subtitle = "2015-11 to 2016-08")
+
+
+data(stop_words)
+
+tidy_subjects <- history_logs %>%
+  unnest_tokens(word, subject) %>%
+  anti_join(stop_words)
+#> Joining, by = "word"
+
+tidy_subjects
+#> # A tibble: 16,477 x 6
+#>    datetime            commit   parents   author       branch word
+#>    <chr>               <fct>    <list>    <chr>         <int> <chr>
+#>  1 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 merge
+#>  2 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 pull
+#>  3 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 request
+#>  4 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 2491
+#>  5 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 tidyverse
+#>  6 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 tidyeval
+#>  7 2018-03-22 18:42:25 3c9c504f <chr [2]> Lionel Henry      1 facets
+#>  8 2018-03-22 17:55:23 449bc039 <chr [1]> Lionel Henry      2 remove
+#>  9 2018-03-22 17:55:23 449bc039 <chr [1]> Lionel Henry      2 dependency
+#> 10 2018-03-22 17:55:23 449bc039 <chr [1]> Lionel Henry      2 plyr
+#> # ... with 16,467 more rows
+#>
+#>
+#>
+#>
+
+tidy_subjects %>%
+  count(word) %>%
+  top_n(10, n) %>%
+  mutate(word = fct_reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(aes(fill = n), show.legend = FALSE) +
+  coord_flip() +
+  theme_minimal() +
+  ggtitle("Most-used words in ggplot2 commit subjects") +
+  labs(x = NULL, y = "Word frequency", caption = "Post by @drsimonj")
+
+
+history_logs %>%
+  select(commit, author, subject) %>%
+  unnest_tokens(bigram, subject, token = "ngrams", n = 2) %>%
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  filter(word1 == "fix") %>%
+  anti_join(stop_words, by = c("word2" = "word")) %>%
+  count(word2, sort = TRUE)
+#> # A tibble: 223 x 2
+#>    word2             n
+#>    <chr>         <int>
+#>  1 bug              57
+#>  2 typo             37
+#>  3 geom             11
+#>  4 guides           11
+#>  5 bugs              7
+#>  6 doc               7
+#>  7 boxplot           6
+#>  8 axis              5
+#>  9 documentation     5
+#> 10 dumb              5
+#> # ... with 213 more rows
+
+
+#+ include=T, echo=F, results="asis"
+tac <- Sys.time()
+cat(sprintf("**END** %s %s@%s %s %f mins\n\n", Sys.time(), Sys.info()["login"],
+            Sys.info()["nodename"], basename(Script.Name), difftime(tac,tic,units = "mins")))
