aafc34f9b4299f66ed28c9fcff8092e6c4567d9b
Natsis Thanasis <natsisathan@gmail.com>
2025-01-25 09:58:50 +0200

    Commit 2025-01-25 09:58

---
+++ git_analysys.R
@@ -0,0 +1,45 @@
+# /* #!/usr/bin/env Rscript */
+# /* Copyright (C) 2025 Athanasios Natsis <natsisphysicist@gmail.com> */
+#' ---
+#' title:         "Plot git stats"
+#' author:        "Natsis Athanasios"
+#' institute:     "AUTH"
+#' affiliation:   "Laboratory of Atmospheric Physics"
+#' documentclass: article
+#' classoption:   a4paper,oneside
+#' fontsize:      10pt
+#' geometry:      "left=0.5in,right=0.5in,top=0.5in,bottom=0.5in"
+#'
+#' link-citations:  yes
+#' colorlinks:      yes
+#'
+#' header-includes:
+#' - \usepackage{caption}
+#' - \usepackage{placeins}
+#' - \captionsetup{font=small}
+#'
+#' output:
+#'   bookdown::pdf_document2:
+#'     number_sections:  no
+#'     fig_caption:      no
+#'     keep_tex:         no
+#'     latex_engine:     xelatex
+#'     toc:              yes
+#'     toc_depth:        4
+#'     fig_width:        8
+#'     fig_height:       5
+#'   html_document:
+#'     toc:        true
+#'     fig_width:  7.5
+#'     fig_height: 5
+#'
+#' date: "`r format(Sys.time(), '%F')`"
+#'
+#' ---
+
+
+
+
+# https://www.r-bloggers.com/2018/03/guide-to-tidy-git-analysis/
+
+
